4a0cc78316a5ec156df275d78a7f280d8a4718e6
cyberrat <joojoquartey11@gmail.com>
2020-04-17 12:06:24 +0000

    feat: sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,13 @@ const Footer = props => {
       <footer
         style={{
           backgroundColor: 'black',
-          color: 'white',
+          color: '#f4f6fa',
           padding: '3rem',
           width: '100vw !important',
           display: 'flex',
           justifyContent: 'center',
           margin: 0,
+          position: 'sticky',
         }}
       >
         © {new Date().getFullYear()}, Made With ❤ by Joojo Quartey
